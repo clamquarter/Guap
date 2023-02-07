f813af778a920a318df1f242c9fbc0ecf85d0f9d
India Doria <60370426+clamquarter@users.noreply.github.com>
2023-02-07 13:34:42 -0500

    Initial Commit

---
+++ Guap/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Guap
+//
+//  Created by India Doria on 2/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Guap/GuapApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuapApp.swift
+//  Guap
+//
+//  Created by India Doria on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct GuapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
