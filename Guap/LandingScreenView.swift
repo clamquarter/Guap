bcac18d3a5414a8ffd73d7754ed3812f5b93f84b
Lisa Wexton <lisa.wexton@gmail.com>
2023-02-09 16:37:31 -0500

    Added pay date logic

--- Guap/LandingScreenView.swift
+++ Guap/LandingScreenView.swift
@@ -9,14 +9,45 @@ import Foundation
 import SwiftUI
 
 struct LandingScreenView: View {
+    @State var date = Date()
+    @State var payday = ""
     var body: some View {
         ZStack{
             backgroundColor
             ScrollView {
                 Text(Date(), style: .date)
-                Text("Welcome to Guap.")
+                Text("Next Payday")
+                
+                Text(paySchedule(payCycle:.weekly))
+                DatePicker ("When's your next payday?", selection: $date, displayedComponents: [.date])
             }
         }
         
+    }
+}
+enum PayCycle {
+    case weekly, biweekly
+    
+
+}
+func paySchedule (payCycle: PayCycle) -> Int{
+    
+
+switch payCycle {
+case .weekly:
+    return 7
+case .biweekly:
+    return 14
+default:
+    return 0
+    
+//case .semimonthly:
+
+//case .monthly:
+}
+}
+struct LandingScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandingScreenView()
     }
 }
